4b95f96b23ef11425f0f8f0ccc3bfc900c8f0a03
alex cornellier <alex@cornellier.com>
2021-11-20 11:54:17 -0500

    part 10

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+
+public class Sign : MonoBehaviour
+{
+    public string dialog;
+    public GameObject dialogBox;
+    public TextMeshProUGUI dialogText;
+
+    bool playerInRange;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if (dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+}
