edf14beb496562ab510d33047f6446466efa4da4
alex cornellier <alex@cornellier.com>
2021-11-20 00:53:59 -0500

    part 8

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject virtualCamera;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCamera.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCamera.SetActive(false);
+        }
+    }
+}
